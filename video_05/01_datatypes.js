40fca0e3ceba20876f4544b04c3333b29cd6235c
Aaryan Karadkar <72786591+Aryan756-web@users.noreply.github.com>
2025-11-17 06:21:33 +0000

    Datatypes

---
+++ video_05/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello")we are using node js not browser
+
+console.log(3 + 3); 
+
+console.log("hitesh");
+
+let name = "hitesh"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stanalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof null); //output -> object
+console.log(typeof undefined); // output -> undefined
+
+
